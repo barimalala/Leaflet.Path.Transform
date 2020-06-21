d6eb07623c393f739d4fa8207de09e53d1b7cccb
tiavina-mika <ralainirinatiavina@gmail.com>
2020-06-21 12:16:39 +0300

    change svg options styles

--- src/Path.Transform_3.js
+++ src/Path.Transform_3.js
@@ -74,12 +74,12 @@ L.Handler.PathTransform = L.Handler.extend({
       centering:false,
 
       handlerOptions: {
-        radius:      5,
-        fillColor:   '#ffffff',
-        color:       '#202020',
+        radius:      10,
+        fillColor:   '#0E203A',
+        color:       '#fff',
         fillOpacity: 1,
-        weight:      2,
-        opacity:     0.7,
+        weight:      4,
+        opacity:     1,
         setCursor:   true
       },
       transform:     true,
@@ -861,7 +861,7 @@ L.Handler.PathTransform = L.Handler.extend({
     );
     this._draggablePt = new L.marker(handlerPosition,{
       icon:L.divIcon({
-              className: 'zoom-icon',
+              className: 'leaflet-draggable-icon',
               iconSize: [25, 25],
               html: '<svg stroke="#fff" fill="#fff" stroke-width="10" viewBox="0 0 512 512" height="25" width="25" xmlns="http://www.w3.org/2000/svg"><path d="M475.9 246.2l-79.4-79.4c-5.4-5.4-14.2-5.4-19.6 0l-.2.2c-5.4 5.4-5.4 14.2 0 19.6l54.9 54.9-161.8.5.5-161.8 54.9 54.9c5.4 5.4 14.2 5.4 19.6 0l.2-.2c5.4-5.4 5.4-14.2 0-19.6l-79.4-79.4c-5.4-5.4-14.2-5.4-19.6 0l-79.4 79.4c-5.4 5.4-5.4 14.2 0 19.6l.2.2c5.4 5.4 14.2 5.4 19.6 0l54.9-54.9.5 161.8-161.8-.5 54.9-54.9c5.4-5.4 5.4-14.2 0-19.6l-.2-.2c-5.4-5.4-14.2-5.4-19.6 0l-79.4 79.4c-5.4 5.4-5.4 14.2 0 19.6l79.4 79.4c5.4 5.4 14.2 5.4 19.6 0l.2-.2c5.4-5.4 5.4-14.2 0-19.6L80 270.5l161.8-.5-.5 161.8-54.9-54.9c-5.4-5.4-14.2-5.4-19.6 0l-.2.2c-5.4 5.4-5.4 14.2 0 19.6l79.4 79.4c5.4 5.4 14.2 5.4 19.6 0l79.4-79.4c5.4-5.4 5.4-14.2 0-19.6l-.2-.2c-5.4-5.4-14.2-5.4-19.6 0l-54.9 54.9-.5-161.8 161.8.5-54.9 54.9c-5.4 5.4-5.4 14.2 0 19.6l.2.2c5.4 5.4 14.2 5.4 19.6 0l79.4-79.4c5.5-5.4 5.5-14.2 0-19.6z"></path></svg>'
           })
